3f54312d62ec0c00e4b1e7e953d7505b314e177f
Pavol Pitonak <pavol@pitonak.com>
2023-08-04 23:10:47 +0200

    tutorial: signed provenance

--- test2.sh
+++ test2.sh
@@ -2,34 +2,29 @@
 
 set +x
 
-#oc patch tektonconfig config --type=merge -p='{"spec":{"chain":{"artifacts.taskrun.format":"slsa/v1","artifacts.taskrun.storage":"tekton","artifacts.oci.storage":""}}}'
-oc patch tektonconfig config --type=merge -p='{"spec":{"chain":{"artifacts.taskrun.format":"in-toto","artifacts.taskrun.storage":"tekton","artifacts.oci.storage":""}}}'
-#oc patch tektonconfig config --type=merge -p='{"spec":{"chain":{"artifacts.taskrun.format":"in-toto","artifacts.taskrun.storage":"tekton"}}}'
-
 oc delete secret signing-secrets -n openshift-pipelines --ignore-not-found
+COSIGN_BIN=cosign-2.0.1
 
-COSIGN_PASSWORD=xxx cosign generate-key-pair k8s://openshift-pipelines/signing-secrets
+COSIGN_PASSWORD="" $COSIGN_BIN generate-key-pair k8s://openshift-pipelines/signing-secrets
+echo "=============="
 #skopeo generate-sigstore-key --output-prefix test-key --passphrase-file passphrase
 #base64 test-key.pub > test-key-b64.pub
 #base64 test-key.private > test-key-b64.private
 #cat passphrase | base64 > test-key-b64.passphrase
 
-echo "Removing Chains controller pod"
-kubectl delete pod -n openshift-pipelines -l app=tekton-chains-controller
-echo "Waiting until new Chains controller pod becomes ready"
-oc wait --for=condition=Ready pod -l app=tekton-chains-controller -n openshift-pipelines --timeout=5m
+oc patch tektonconfig config --type=merge -p='{"spec":{"chain":{"artifacts.taskrun.format":"slsa/v1","artifacts.taskrun.storage":"oci","transparency.enabled":"true"}}}'
 
-echo "Secrets in openshift-pipelines"
-oc get secrets -n openshift-pipelines | grep signing
+#echo "=============="
+#echo "Logging in as non-admin user"
+#oc login -u user -p user > /dev/null
 
-#oc login -u user -p user
-#podman login -u user -p $(oc whoami -t) --tls-verify=false --authfile ~/.docker/config.json $REGISTRY
-
-NAMESPACE=chainstest-$RANDOM
+NAMESPACE=chainstest
 oc new-project $NAMESPACE
+sleep 10
 
 REGISTRY=quay.io
-REPO=ppitonak/chainstest:$(date +"%m%d%H%M%S")
+REPO=ppitonak/chainstest
+TAG=$(date +"%m%d%H%M%S")
 
 oc delete secret quay --ignore-not-found
 oc create secret generic quay --from-file=.dockerconfigjson=quay_ppitonak_robot.json --type=kubernetes.io/dockerconfigjson
@@ -37,16 +32,29 @@ oc secrets link pipeline quay --for=pull,mount
 
 oc apply -f kaniko.yaml
 
-echo "Image: $REGISTRY/$REPO"
+echo "Image: $REGISTRY/$REPO:$TAG"
 
-tkn task start --param IMAGE=$REGISTRY/$REPO --use-param-defaults --workspace name=source,emptyDir="" --workspace name=dockerconfig,secret=quay kaniko-chains --showlog
+tkn task start --param IMAGE=$REGISTRY/$REPO:$TAG --use-param-defaults --workspace name=source,emptyDir="" --workspace name=dockerconfig,secret=quay kaniko-chains --showlog
 
 sleep 5
 
+IMAGE_DIGEST=$(tkn tr describe --last -o json | jq -r  '.status.results[] | select(.name | test("IMAGE_DIGEST")).value' | cut -d":" -f2)
+echo "Waiting 90 seconds for images to appear in image registry"
+sleep 90
 echo "=============="
-export TASKRUN_UID=$(tkn tr describe --last -o  jsonpath='{.metadata.uid}')
-echo "tkn tr describe --last -o jsonpath=\"{.metadata.annotations.chains\.tekton\.dev/signature-taskrun-$TASKRUN_UID}\" | base64 -d > sig"
-tkn tr describe --last -o jsonpath="{.metadata.annotations.chains\.tekton\.dev/signature-taskrun-$TASKRUN_UID}" | base64 -d > sig
-echo "cosign verify-blob-attestation --insecure-ignore-tlog --key cosign.pub --signature sig --type slsaprovenance --check-claims=false /dev/null"
-cosign verify-blob-attestation --insecure-ignore-tlog --key cosign.pub --signature sig --type slsaprovenance --check-claims=false /dev/null
+
+echo "$COSIGN_BIN verify --key cosign.pub $REGISTRY/$REPO@sha256:$IMAGE_DIGEST"
+$COSIGN_BIN verify --key cosign.pub $REGISTRY/$REPO@sha256:$IMAGE_DIGEST
+echo "=============="
+
+echo "$COSIGN_BIN verify-attestation --key cosign.pub --type slsaprovenance $REGISTRY/$REPO@sha256:$IMAGE_DIGEST"
+$COSIGN_BIN verify-attestation --key cosign.pub --type slsaprovenance $REGISTRY/$REPO@sha256:$IMAGE_DIGEST
+echo "=============="
+
+echo "rekor-cli search --format json --sha $IMAGE_DIGEST | jq -r '.UUIDs[0]'"
+REKOR_UUID=$(rekor-cli search --format json --sha $IMAGE_DIGEST | jq -r '.UUIDs[0])'
+echo "=============="
+
+echo "rekor-cli get --uuid $REKOR_UUID --format json | jq -r .Attestation | jq"
+rekor-cli get --uuid $REKOR_UUID --format json | jq -r .Attestation | jq
 
