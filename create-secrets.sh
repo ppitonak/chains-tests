c23b359c425e5f17cc26bd3a352acc078873410b
Pavol Pitonak <pavol@pitonak.com>
2023-11-24 13:56:56 +0100

    use static signing key

--- create-secrets.sh
+++ create-secrets.sh
@@ -8,15 +8,17 @@ COSIGN_BIN=cosign-2.2.1
 oc delete secret signing-secrets -n openshift-pipelines --ignore-not-found
 sleep 15
 
-COSIGN_PASSWORD=xxx $COSIGN_BIN generate-key-pair k8s://openshift-pipelines/signing-secrets
+oc create -f signing-secrets.yaml
+#COSIGN_PASSWORD=xxx $COSIGN_BIN generate-key-pair k8s://openshift-pipelines/signing-secrets
 sleep 15
+
 #skopeo generate-sigstore-key --output-prefix test-key --passphrase-file passphrase
 #base64 test-key.pub > test-key-b64.pub
 #base64 test-key.private > test-key-b64.private
 #cat passphrase | base64 > test-key-b64.passphrase
 
 # TODO oc rollout should be the right approach
-oc delete deployment tekton-chains-controller -n openshift-pipelines
+#oc delete deployment tekton-chains-controller -n openshift-pipelines
 #oc rollout restart deployment/tekton-chains-controller -n openshift-pipelines
 #oc delete po -n openshift-pipelines -l app=tekton-chains-controller
 
