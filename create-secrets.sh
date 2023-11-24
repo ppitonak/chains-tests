4dca9e263774217e6db045204448428ef121d7e0
Pavol Pitonak <pavol@pitonak.com>
2023-11-24 14:21:32 +0100

    oc apply instead of create

--- create-secrets.sh
+++ create-secrets.sh
@@ -5,10 +5,10 @@ set +x
 #COSIGN_BIN=cosign-1.13.1
 COSIGN_BIN=cosign-2.2.1
 
-oc delete secret signing-secrets -n openshift-pipelines --ignore-not-found
+#oc delete secret signing-secrets -n openshift-pipelines --ignore-not-found
 sleep 15
 
-oc create -f signing-secrets.yaml
+oc apply -f signing-secrets.yaml
 #COSIGN_PASSWORD=xxx $COSIGN_BIN generate-key-pair k8s://openshift-pipelines/signing-secrets
 sleep 15
 
